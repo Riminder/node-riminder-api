3bb6d13a956704af12d22c9c406e7e58d996470e
Corentin Godeau <corentin.godeau37@gmail.com>
2018-06-19 16:51:24 +0200

    refactor: removed obsolete code

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,6 @@
 import { httpRequest } from "./http";
 import defaults from "./defaults";
 
-export const create = (object: string, data: any) => {
-  let url = `${defaults.API_URL}/${object}`;
-  let body = JSON.stringify(data);
-  let options = {
-    method: "POST",
-    body,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
-  return httpRequest(url, options);
-};
-
 export const generateURLParams = (options: any) => {
   return options ? Object.keys(options).map(key => {
     if (options[key] instanceof Array) {
