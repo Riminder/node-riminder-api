6f59bfb6238f0655c9aea17c38c5d5177442f9bc
Corentin Godeau <corentin.godeau37@gmail.com>
2018-06-20 16:54:10 +0200

    fix(utils): array form were wrong

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 export const generateURLParams = (options: any) => {
   return options ? Object.keys(options).map(key => {
     if (options[key] instanceof Array) {
-      return `${key}=[${options[key]}]`;
+      return `${key}=[${options[key].map((elem: string) => `"${elem}"`).join(",")}]`;
     }
     return `${key}=${options[key]}`;
   }).join("&") : null;
 };
+
