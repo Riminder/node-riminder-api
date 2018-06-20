b9f14c3ca6d5b23b0a676b730ca7e4b303073b83
Corentin Godeau <corentin.godeau37@gmail.com>
2018-06-20 10:55:16 +0200

    feat(mock): added the possibility to reject

--- __mocks__/fetch-everywhere.js
+++ __mocks__/fetch-everywhere.js
@@ -4,6 +4,9 @@ import { handleRequest } from "./mockedResponse";
 const fetch = (url, opts) => {
   return new Promise((resolve, reject) => {
     const parsedUrl = parse(url);
+    if (opts.reject) {
+      return reject({message: "Error message"});
+    }
     return resolve(handleRequest(parsedUrl, opts, opts.error ? 400 : 200));
   });
 };
