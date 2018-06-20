7f33ea6de3f717b2abcdc8dba924f15daed24420
Corentin Godeau <corentin.godeau37@gmail.com>
2018-06-20 10:45:05 +0200

    refactor: switched to object behaviour

--- src/objects.ts
+++ src/objects.ts
@@ -10,51 +10,68 @@ import { generateURLParams } from "./utils";
 import defaults from "./defaults";
 import { httpRequest, httpPostRequest, httpPatchRequest } from "./http";
 import { ReadStream } from "fs";
+import { Riminder } from "./index";
 
-export default {
-  getSources: () => {
-    return httpRequest(`${defaults.API_URL}/sources`);
-  },
-  getSource: (id: string) => {
-    return httpRequest(`${defaults.API_URL}/source?source_id=${id}`);
-  },
-  getProfiles: (options: ProfilesOptions) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/profiles?${urlParams}`);
-  },
-  getProfile: (options: ProfileOptionIdOrReference) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/profile?${urlParams}`);
-  },
-  getFilters: () => {
-    return httpRequest(`${defaults.API_URL}/filters`);
-  },
-  getFilter: (options: FilterIdOrReference) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/filter?${urlParams}`);
-  },
-  createResumeForProfile: (data: ProfileUpload, file: ReadStream) => {
-    const url = `${defaults.API_URL}/profile`;
-    return httpPostRequest(url, data, file);
-  },
-  getProfileDocuments: (options: ProfileOptionIdOrReference) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/profile/documents?${urlParams}`);
-  },
-  getProfileParsing: (options: ProfileOptionIdOrReference) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/profile/parsing?${urlParams}`);
-  },
-  getProfileScoring: (options: ProfileOptionIdOrReference) => {
-    const urlParams = generateURLParams(options);
-    return httpRequest(`${defaults.API_URL}/profile/scoring?${urlParams}`);
-  },
-  updateProfileStage: (data: StagePatch) => {
-    let url = `${defaults.API_URL}/profile/stage`;
-    return httpPatchRequest(url, data);
-  },
-  updateProfileRating: (data: RatingPatch) => {
-    let url = `${defaults.API_URL}/profile/rating`;
-    return httpPatchRequest(url, data);
+export class Objects {
+  apiKey: string;
+  constructor(riminder: Riminder) {
+    this.apiKey = riminder.API_Key;
   }
-};
+
+  getSources() {
+    return httpRequest(`${defaults.API_URL}/sources`, this.apiKey);
+  }
+
+  getSource(id: string) {
+    return httpRequest(`${defaults.API_URL}/source?source_id=${id}`, this.apiKey);
+  }
+
+  getProfiles(options: ProfilesOptions) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/profiles?${urlParams}`, this.apiKey);
+  }
+
+  getProfile(options: ProfileOptionIdOrReference) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/profile?${urlParams}`, this.apiKey);
+  }
+
+  getFilters() {
+    return httpRequest(`${defaults.API_URL}/filters`, this.apiKey);
+  }
+
+  getFilter(options: FilterIdOrReference) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/filter?${urlParams}`, this.apiKey);
+  }
+
+  createResumeForProfile(data: ProfileUpload, file: ReadStream) {
+    const url = `${defaults.API_URL}/profile`;
+    return httpPostRequest(url, this.apiKey, data, file);
+  }
+
+  getProfileDocuments(options: ProfileOptionIdOrReference) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/profile/documents?${urlParams}`, this.apiKey);
+  }
+
+  getProfileParsing(options: ProfileOptionIdOrReference) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/profile/parsing?${urlParams}`, this.apiKey);
+  }
+
+  getProfileScoring(options: ProfileOptionIdOrReference) {
+    const urlParams = generateURLParams(options);
+    return httpRequest(`${defaults.API_URL}/profile/scoring?${urlParams}`, this.apiKey);
+  }
+
+  updateProfileStage(data: StagePatch) {
+    let url = `${defaults.API_URL}/profile/stage`;
+    return httpPatchRequest(url, this.apiKey, data);
+  }
+
+  updateProfileRating(data: RatingPatch) {
+    let url = `${defaults.API_URL}/profile/rating`;
+    return httpPatchRequest(url, this.apiKey, data);
+  }
+}
